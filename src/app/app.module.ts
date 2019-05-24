f4c8a3c23995ccd13b6473a167408bcdc5cad02f
StellaShoihat <stellashoihat93@gmail.com>
2019-05-24 22:22:25 +0300

    upload to get all the done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
@@ -8,20 +9,31 @@ import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
+import { SignupPage } from './../pages/signup/signup';
+import { LoginPage } from './../pages/login/login';
+import { WelcomePage } from './../pages/welcome/welcome';
+
+import { SignaturePadModule } from 'angular2-signaturepad';
+
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import { AuthServiceProvider } from '../providers/auth-service/auth-service';
+import { HttpModule } from '@angular/http';
 @NgModule({
   declarations: [
     MyApp,
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    SignupPage,
+    LoginPage,
+    WelcomePage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -29,12 +41,16 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    SignupPage,
+    LoginPage,
+    WelcomePage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthServiceProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
+import { AuthServiceProvider } from './../../providers/auth-service/auth-service';
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController ,App} from 'ionic-angular';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,41 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  userDetails : any;
+  responseData: any;
+
+  userPostData = {"user_id":"","token":""};
+  constructor(public navCtrl: NavController , public app:App,  public authService:AuthServiceProvider) {
+
+
+    const data = JSON.parse(localStorage.getItem('userData'));
+    this.userDetails = data.userData;
+
+    this.userPostData.user_id = this.userDetails.user_id;
+    this.userPostData.token = this.userDetails.token;
 
   }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad HomePage');
+  }
+
+  backToWelcome(){
+    const root = this.app.getRootNav();
+    root.popToRoot();
+ }
+
+ logout(){
+      localStorage.clear();
+      setTimeout(() => this.backToWelcome(), 1000);
+ }
+
+
+   /* logout(){
+      // Remove API token
+      const root = this.app.getRootNav();
+      root.popToRoot();
+  }
+  */
 
 }
