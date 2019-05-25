998a21833a24997ac176678ad438e0d6752114f3
StellaShoihat <stellashoihat93@gmail.com>
2019-05-25 11:23:07 +0300

    loginPage
    add new page with functions

---
+++ src/pages/login/login.ts
@@ -0,0 +1,73 @@
+import { AuthServiceProvider } from './../../providers/auth-service/auth-service';
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams ,ToastController} from 'ionic-angular';
+import { TabsPage } from '../tabs/tabs';
+import { HomePage } from '../home/home';
+
+/**
+ * Generated class for the LoginPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+
+  resposeData : any;
+  userData = {"username":"", "password":""};
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,public authService: AuthServiceProvider, private toastCtrl:ToastController) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+
+  //
+
+  /*login(){
+    // Your app login API web service call triggers
+    this.navCtrl.push(HomePage, {}, {animate: true});
+  }*/
+  login(){
+    if(this.userData.username && this.userData.password){
+     this.authService.postData(this.userData, "login").then((result) =>{
+     this.resposeData = result;
+     console.log(this.resposeData);
+     if(this.resposeData.userData){
+      localStorage.setItem('userData', JSON.stringify(this.resposeData) )
+     this.navCtrl.push(TabsPage);
+   }
+   else{
+     this.presentToast("Please give valid username and password");
+   }
+
+
+
+     }, (err) => {
+       //Connection failed message
+       console.log("login post error!");
+     });
+    }
+    else{
+     this.presentToast("Give username and password");
+    }
+
+   }
+
+
+   presentToast(msg) {
+     let toast = this.toastCtrl.create({
+       message: msg,
+       duration: 2000
+     });
+     toast.present();
+   }
+
+}
---
+++ src/providers/auth-service/auth-service.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import {  Injectable } from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
+/*
+  Generated class for the AuthServiceProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+
+let apiUrl = 'http://localhost:8000/api/';
+
+@Injectable()
+export class AuthServiceProvider {
+
+  constructor(public http: Http) {
+    console.log('Hello AuthServiceProvider Provider');
+  }
+
+
+
+
+
+  postData(credentials, type) {
+    return new Promise((resolve, reject) => {
+      let headers = new Headers();
+
+      this.http.post(apiUrl + type, JSON.stringify(credentials), {headers: headers})
+        .subscribe(res => {
+          resolve(res.json());
+        }, (err) => {
+          reject(err);
+        });
+    });
+
+
+}
+}
